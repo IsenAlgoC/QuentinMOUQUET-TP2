6c521ca6afe4bbf6e814625c2e7567a842fc73d5
Quentin MOUQUET <quentin.mouquet@isen.yncrea.fr>
2020-09-23 16:35:17 +0200

    Exo 3 fin

--- TP2/Exo3/bisextile.c
+++ TP2/Exo3/bisextile.c
@@ -1,1 +1,26 @@
-#include 
+#include <stdlib.h>
+#include <stdio.h>
+
+main() {
+	int annee = 10001;
+	while (annee > 10000) {
+		printf("\nEntrez une année ");
+		scanf_s("%d", &annee);
+	}
+	if (annee % 4 == 0) {
+		if (annee % 100 == 0) {
+			if (annee % 400 == 0) {
+				printf("\n%d est bisextile", annee);
+			}
+			else {
+				printf("\n%d n'est pas bisextile", annee);
+			}
+		}
+		else {
+			printf("\n%d est bisextile", annee);
+		}
+	}
+	else {
+		printf("\n%d n'est pas bisextile", annee);
+	}
+}
